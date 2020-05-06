b46b6aa25cdfa66b8bd03605a44518b42a54a18f
Amith's MacBook <amithkumarnarayan1@gmail.com>
2020-05-07 00:32:15 +0100

    Initial Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Amith Kumar Narayan on 07/05/2020.
+//  Copyright © 2020 Narayan inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
